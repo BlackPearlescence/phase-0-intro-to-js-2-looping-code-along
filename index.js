d9fbd71f6a879471fb477990c8ad0dcc37cb72cf
Sani Rogers <sanirogers138@gmail.com>
2023-01-02 16:37:16 -0500

    commit tree 2bf32b2211b2260f31fe5c21de2057063ee75d49

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(recipients,eventName){
+    const giftMsg = [];
+    for(let i = 0; i < recipients.length; i++){
+        giftMsg[i] = `Thank you, ${recipients[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return giftMsg;
+}
+
+function countDown(integer){
+    while(integer > -1){
+        console.log(integer);
+        integer -= 1;
+    }
+}
